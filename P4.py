00daf211f579331994b664bfcf90a4243d08bfe5
lTJShowerHandel <blanesantilli@gmail.com>
2025-04-12 17:47:23 -0600

    Update P4.py
    adding the code for bar chart creation

--- P4.py
+++ P4.py
@@ -9,6 +9,19 @@ iUserInput = int(input(f'If you want to import data, enter 1. If you want to see
 # Set excel file to variable
 file_path = 'Retail_Sales_Data.xlsx'
 
+<<<<<<< Updated upstream
+=======
+# Create engine outside of if statements
+username = 'postgres'
+# this password will change per whoever is running it
+password = '12345'
+host = 'localhost'
+port = '5432'
+database = 'is303'
+
+engine = create_engine(f'postgresql+psycopg2://{username}:{password}@{host}:{port}/{database}')
+
+>>>>>>> Stashed changes
 # Logic for import data input (1)
 if iUserInput == 1:
 
@@ -71,6 +84,44 @@ elif iUserInput == 2:
     # avg calculation
     # total units calculation
     # display bar chart
+    # use group by to get one row for each product then calc the sum of the total prices for each product 
+    dfSales = dfCategory.groupby('product')['total_price'].sum()
+
+    # create the actual bar chart 
+    dfSales.plot(kind= 'bar') 
+
+    # title
+    plot.title(f"Total Sales in {SelectedCategory}")
+# label for the x axis
+    plot.xlabel("Product")
+# label for the y axis
+    plot.ylabel("Total Sales")
+    # makes the chart appear on the screen
+    plot.show()
+
+
+    """
+    # now start making the barchart and table
+    # this was the start of the alternate way of doing things but I ended up going with Greg's method as it was easier
+    # gives the table if it exists already 
+    sqlDropTableIfExists = "DROP TABLE IF EXISTS sale";
+    # now store the variable that holds the cursor
+    cursor = conn.cursor()
+    # run the tables commands if they exists/create one if it doesn't exist
+    cursor.execute(sqlDropTableIfExists)
+    # commit the changes from the buffer to the database
+    conn.commit()
+    """
+
+
+
+#don't edit DEFAULT becasue that will be the primary key/id number  but this command will insert the records into the table 
+# %s is simply a placeholder in a sql statement to pass in values into the statement
+    sqlInsert = "INSERT INTO sales VALUES (DEFAULT, %s, %s)"
+
+
+
+
 
 # Exit program
 else:
